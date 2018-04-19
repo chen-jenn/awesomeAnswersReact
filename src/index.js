ed33114513e840331c079974b7b96fcaaab76f50
jennifer <jenn.chen@live.ca>
2018-04-19 14:13:57 -0700

    Add solution for AnswerDetails

--- src/index.js
+++ src/index.js
@@ -3,7 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>Hello World</h1>, //calling React.createElement, first argument is the h1
-  document.getElementById('root')
+// Create a Component------------------------------------------------------
+function QuestionDetails(props){//function needs to have a capital letter otherwise React thinks it is just html
+  return (
+    <div className="QuestionDetails">
+      <h1>What is your favourite colour?</h1>
+      <p>Red, green, blue, magenta, yellow</p>
+      <p>By Bridge Troll</p>
+      <p><strong>View Count: </strong>111</p>
+      <p><strong>Created</strong> 2018-01-01</p>
+    </div>
+  )
+}
+
+function AnswerDetails(props){
+  return ( //this is technically optional but can just className named after your function name
+    <div className="AnswerDetails">
+      <h1>Answers</h1>
+      <p>yellow</p>
+      <p>By: Doge Shiba</p>
+      <p><strong>Created:</strong> 2018-01-01</p>
+    </div>
+  )
+}
+
+//Rendering Views------------------------------------------------------
+ReactDOM.render(<QuestionDetails />,
+  document.getElementById('root')//calling React.createElement FUNCTION
 );
+
+ReactDOM.render(<AnswerDetails />,
+  document.getElementById('answers')
+);
+
 registerServiceWorker();
