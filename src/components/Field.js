aae711a4cb3eebe5f29f0508f4094dd03b55b69b
jennifer <jenn.chen@live.ca>
2018-04-19 15:27:37 -0700

    Add demo for styling Fields and Main

--- src/components/Field.js
+++ src/components/Field.js
@@ -3,7 +3,12 @@ import React from "react" //whenever you use JSX you must import React so that t
 function Field(props){ //name that describes well what it is doing/showing; used to refactor repetitive code in your other components and helps with reusing styles
   return(
     <p className="Field">
-      <strong>{props.name}</strong> <em>{props.value}</em>
+      <strong style={{
+        color: "white",
+        backgroundColor: "maroon",
+        borderRadius: "3px",
+        padding: "0 4px"
+      }}>{props.name}</strong> <em>{props.value}</em>
     </p>
   );
 }
