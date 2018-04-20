8c9e929de10ae9ac70e49d9f91d75c3e6956515a
jennifer <jenn.chen@live.ca>
2018-04-20 14:47:35 -0700

    Add delete button for question show page

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -24,24 +24,42 @@ import detailedQuestion from '../data/detailedQuestion';
 //Refactoring the above as a class
 class QuestionShowPage extends Component {
   constructor(props){
-    super(props); //must always say super(props) otherwise errors 
+    super(props); //must always say super(props) otherwise errors
     this.state = {
       question: detailedQuestion //store your data here
     }
+
+    this.deleteQuestion = this.deleteQuestion.bind(this)
+  }
+
+  deleteQuestion(){
+    this.setState({
+      question: {}
+    });
   }
 
   render(){
-    return(
-      <main className="QuestionShowPage">
-        <QuestionDetails
-          {...this.state.question}
-        />
-        <h2>Answers</h2>
-        <AnswerList
-          answers={this.state.question.answers}
-        />
-      </main>
-    )
+    if (!this.state.question.id){
+      return (
+        <main className="QuestionShowPage">
+          <h2>Question does not exist</h2>
+        </main>
+      );
+    } else {
+      return(
+        <main className="QuestionShowPage">
+          <QuestionDetails
+            {...this.state.question}
+          />
+          <button onClick={this.deleteQuestion}>Delete</button>
+
+          <h2>Answers</h2>
+          <AnswerList
+            answers={this.state.question.answers}
+          />
+        </main>
+      )
+    }
   }
 }
 
