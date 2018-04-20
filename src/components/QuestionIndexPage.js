db6d5f6cd9966df67c910bff5c74bb1f29b77ade
jennifer <jenn.chen@live.ca>
2018-04-20 14:29:00 -0700

    Add alternate solution to Delete for Questions Index using a closure

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -14,17 +14,20 @@ class QuestionIndexPage extends Component {
     }
 
     //Methods that are used as callbacks will no longer be owned by their instance once they're called. Meaning that their 'this' will be either undefined or 'Window'. Use 'bind' on the method to permanently set its 'this' to the instance's 'this'
-    this.deleteQuestion = this.deleteQuestion.bind(this);
+    // this.deleteQuestion = this.deleteQuestion.bind(this);
   }
 
-  deleteQuestion(event){ //a prototype method; a method not done on the object, but on the object's prototype
-    const{currentTarget} = event;
-    const{questions} = this.state;
+  //passing the arrow function that deleteQuestion returns
+  //arrow functions keep the 'this' of its surrounding block, so you don't even need to bind it; it's like it is pre-bound
+  deleteQuestion(questionId){ //a prototype method; a method not done on the object, but on the object's prototype
+    return () => {
+      const{questions} = this.state;
 
-    this.setState({ //use this whenever you want to change a state. pass an object; that object will be merged to the current state
-      // questions: [] //this would replace your entire state with an empty array
-      questions: questions.filter(q => q.id !== parseInt(currentTarget.dataset.id, 10))
-    })
+      this.setState({ //use this whenever you want to change a state. pass an object; that object will be merged to the current state
+        // questions: [] //this would replace your entire state with an empty array
+        questions: questions.filter(q => q.id !== questionId)
+      });
+    }
   }
 
   render(){
@@ -43,7 +46,7 @@ class QuestionIndexPage extends Component {
                     justifyContent: "space-between"
                   }}>
                     <Field name="Author" value={question.author.full_name} />
-                    <button data-id={question.id} onClick={this.deleteQuestion}>Delete</button>
+                    <button onClick={this.deleteQuestion(question.id)}>Delete</button>
                   </div>
 
                 </li>
